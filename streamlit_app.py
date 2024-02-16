da5dc87a83d40d956bf95bb39aff6e4e2dac5f5a
unite-deals <unitedealstest1@gmail.com>
2024-02-16 20:23:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ def get_vector_store(text_chunks):
 def get_conversational_chain():
 
     prompt_template = """
-    Answer the question as detailed as possible from the provided context if possible add some key points in list format , make sure to provide all the details, if the answer is not in
+    expert and capable to read multiple languages .Answer the question as detailed as possible from the provided context if possible add some key points in list format , make sure to provide all the details, if the answer is not in
     provided context just say, "answer is not available in the context & add your suggestion is differet subtitle also", don't provide the wrong answer\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
